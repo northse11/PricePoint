c6404958b961b3ff76c4e17161c8fc1deeb49ace
Steven North <northse@mail.uc.edu>
2025-03-14 09:50:12 -0400

    Add project files.

---
+++ PricePoint/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PricePoint
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
